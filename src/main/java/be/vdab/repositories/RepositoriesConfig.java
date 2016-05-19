696c96e8d4ba59552bdec6afe63e089fecc0787e
Yannick Thibos <yannick.thibos@gmail.com>
2016-05-19 15:12:21 +0200

    Fixed.

--- src/main/java/be/vdab/repositories/RepositoriesConfig.java
+++ src/main/java/be/vdab/repositories/RepositoriesConfig.java
@@ -7,10 +7,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @PropertySource("classpath:persoonrepo.properties")
 public class RepositoriesConfig {
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+	    return new PropertySourcesPlaceholderConfigurer();
+	}
 	
 	@Value("${csvFile}")
 	private File csvFile;
